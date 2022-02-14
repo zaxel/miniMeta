7206c3796b4d879a0c797f8b106c6ca8e67560a9
zaxel <sae11.z.alex@gmail.com>
2022-02-14 14:35:39 +0200

    bug when you have to return function in redux devtool connection fixed

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const store = createStore(rootReducer, compose(
     thunk,
     spamFilter
   ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
 ));
 
 ReactDOM.render(
